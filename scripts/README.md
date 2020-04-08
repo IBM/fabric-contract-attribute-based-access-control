f17b74eab7f84864c0e7fb2ae96c47c65015d829
Ann Umberhocker <ann.umberhocker@ibm.com>
2020-04-08 12:16:28 -0500

    Updated scripts

--- scripts/README.md
+++ scripts/README.md
@@ -1,13 +1,22 @@
 # Generic Supply Chain Test Scenario
 
- - create_identities - Set up identities used in the Generic Supply Chain demo:
+ - create_identities - Register identities used in the Generic Supply Chain demo:
     ```
     $ chmod +x create_identities.sh
     $ ./create_identities.sh
+    ```
+ - enroll_identities - Enroll identities into wallet used in the Generic Supply Chain demo:
+    ```
+    $ chmod +x enroll_identities.sh
+    $ ./enroll_identities.sh
+    ```
+ - error_testcase.sh - Run through a sample scenario using curl commands to test error conditions 
+    ```
+    $ chmod +x error_testcase.sh
+    $ ./error_testcase.sh
     ```
  - testcase.sh - Run through a sample scenario using curl commands to create Orders and move an Order through the process.
     ```
     $ chmod +x testcase.sh
     $ ./testcase.sh
     ```
-
--- scripts/create_identities.sh
+++ scripts/create_identities.sh
@@ -6,13 +6,35 @@
 echo "What is the IP address for the API server (default is localhost:3000)?"
 read API_URL
 API_URL=${API_URL:-localhost:3000}
-set -x
+#set -x
 # base64 encoded string added for 'admin:adminpw' to authorization header
+echo 'curl -X POST "${API_URL}/api/register-user" -H "authorization: Basic YWRtaW46YWRtaW5wdw==" -H "Content-Type: application/json" -d "{\"userid\":\"Walmart\",\"password\":\"Walmart\",\"usertype\":\"retailer\"}"'
 curl -X POST "${API_URL}/api/register-user" -H "authorization: Basic YWRtaW46YWRtaW5wdw==" -H "Content-Type: application/json" -d "{\"userid\":\"Walmart\",\"password\":\"Walmart\",\"usertype\":\"retailer\"}"
+echo ""
+echo ""
+echo 'curl -X POST "${API_URL}/api/register-user" -H "authorization: Basic YWRtaW46YWRtaW5wdw==" -H "Content-Type: application/json" -d "{\"userid\":\"HEB\",\"password\":\"HEB\",\"usertype\":\"retailer\"}"'
 curl -X POST "${API_URL}/api/register-user" -H "authorization: Basic YWRtaW46YWRtaW5wdw==" -H "Content-Type: application/json" -d "{\"userid\":\"HEB\",\"password\":\"HEB\",\"usertype\":\"retailer\"}"
+echo ""
+echo ""
+echo 'curl -X POST "${API_URL}/api/register-user" -H "authorization: Basic YWRtaW46YWRtaW5wdw==" -H "Content-Type: application/json" -d "{\"userid\":\"GHFarm\",\"password\":\"GHFarm\",\"usertype\":\"producer\"}"'
 curl -X POST "${API_URL}/api/register-user" -H "authorization: Basic YWRtaW46YWRtaW5wdw==" -H "Content-Type: application/json" -d "{\"userid\":\"GHFarm\",\"password\":\"GHFarm\",\"usertype\":\"producer\"}"
+echo ""
+echo ""
+echo 'curl -X POST "${API_URL}/api/register-user" -H "authorization: Basic YWRtaW46YWRtaW5wdw==" -H "Content-Type: application/json" -d "{\"userid\":\"ABFarm\",\"password\":\"ABFarm\",\"usertype\":\"producer\"}"'
 curl -X POST "${API_URL}/api/register-user" -H "authorization: Basic YWRtaW46YWRtaW5wdw==" -H "Content-Type: application/json" -d "{\"userid\":\"ABFarm\",\"password\":\"ABFarm\",\"usertype\":\"producer\"}"
+echo ""
+echo ""
+echo 'curl -X POST "${API_URL}/api/register-user" -H "authorization: Basic YWRtaW46YWRtaW5wdw==" -H "Content-Type: application/json" -d "{\"userid\":\"FDA\",\"password\":\"FDA\",\"usertype\":\"regulator\"}"'
 curl -X POST "${API_URL}/api/register-user" -H "authorization: Basic YWRtaW46YWRtaW5wdw==" -H "Content-Type: application/json" -d "{\"userid\":\"FDA\",\"password\":\"FDA\",\"usertype\":\"regulator\"}"
+echo ""
+echo ""
+echo 'curl -X POST "${API_URL}/api/register-user" -H "authorization: Basic YWRtaW46YWRtaW5wdw==" -H "Content-Type: application/json" -d "{\"userid\":\"Fedex\",\"password\":\"Fedex\",\"usertype\":\"shipper\"}"'
 curl -X POST "${API_URL}/api/register-user" -H "authorization: Basic YWRtaW46YWRtaW5wdw==" -H "Content-Type: application/json" -d "{\"userid\":\"Fedex\",\"password\":\"Fedex\",\"usertype\":\"shipper\"}"
+echo ""
+echo ""
+echo 'curl -X POST "${API_URL}/api/register-user" -H "authorization: Basic YWRtaW46YWRtaW5wdw==" -H "Content-Type: application/json" -d "{\"userid\":\"UPS\",\"password\":\"UPS\",\"usertype\":\"shipper\"}"'
 curl -X POST "${API_URL}/api/register-user" -H "authorization: Basic YWRtaW46YWRtaW5wdw==" -H "Content-Type: application/json" -d "{\"userid\":\"UPS\",\"password\":\"UPS\",\"usertype\":\"shipper\"}"
+echo ""
+echo ""
+echo 'curl -X POST "${API_URL}/api/register-user" -H "authorization: Basic YWRtaW46YWRtaW5wdw==" -H "Content-Type: application/json" -d "{\"userid\":\"ACustomer\",\"password\":\"ACustomer\",\"usertype\":\"customer\"}"'
 curl -X POST "${API_URL}/api/register-user" -H "authorization: Basic YWRtaW46YWRtaW5wdw==" -H "Content-Type: application/json" -d "{\"userid\":\"ACustomer\",\"password\":\"ACustomer\",\"usertype\":\"customer\"}"
